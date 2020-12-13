fca3170fa556b58c04c8f037f547be532b1f26e7
martyna-dz <martynadziamara14@gmail.com>
2020-12-13 16:11:11 +0100

    added iterator for rules and server commands for movement

--- ChineseCheckersServer/src/main/java/Game.java
+++ ChineseCheckersServer/src/main/java/Game.java
@@ -1,3 +1,5 @@
+import boards.*;
+import rules.*;
 import javax.swing.*;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,11 +13,19 @@ class Game {
     private int playersNumber;
     int rnd;
     Player currentPlayer;
+    GameBoard board = new StandardBoard();
 
     public Game(int playersNumber) {
         this.playersNumber = playersNumber;
         rnd = (new Random().nextInt(playersNumber)) - 1;
     }
+
+    public synchronized void move(int location, Player player) {
+        if (player != currentPlayer) {
+            throw new IllegalStateException("Not your turn");
+        }
+    }
+
 
     class Player implements Runnable {
         int playerId;
@@ -23,6 +33,7 @@ class Game {
         Socket socket;
         Scanner input;
         PrintWriter output;
+        int chosenX, chosenY, newX, newY;
 
         public Player(Socket socket, int playerId) {
             this.socket = socket;
@@ -63,20 +74,96 @@ class Game {
             for (int i = 0; i<players.size(); i++) {
                 if(i == rnd)
                 {
+                    Player playerOne = players.get(i);
+                    players.remove(i);
+                    List<Player> temp = new ArrayList<Player>();
+                    temp.add(playerOne);
+                    playerOne.playerId = 1;
+                    for (int j = 0; j<players.size(); j++)
+                    {
+                        temp.add(players.get(j));
+                        temp.get(j+1).playerId = j+2;
+                    }
+                    players = temp;
+                    temp = null;
                     currentPlayer = this;
+                    break;
                 }
-                players.get(i).output.println("PLAYER It's player's " + rnd + "turn!");
             }
+
         }
 
         private void processCommands() {
             while (input.hasNextLine()) {
                 var command = input.nextLine();
                 if (command.startsWith("MOVE")) {
-                    return;
+                    String [] temp = command.substring(5).split("\\|");
+
+                    chosenX = Integer.parseInt(temp[0]);
+                    chosenY = Integer.parseInt(temp[1]);
+
+                    if (this == currentPlayer) {
+                        // if(isMoveAvailable())
+                        processMoveCommand(chosenX, chosenY);
+                    }
+                    else {
+                        output.println("MESSEGE Not your turn");
+                    }
+                }
+                else if (command.startsWith("CURRENT")) {
+                    String [] temp = command.substring(7).split("\\|");
+                    chosenX = Integer.parseInt(temp[0]);
+                    chosenY = Integer.parseInt(temp[1]);
+
+                    if (this == currentPlayer) {
+
+                        if (board.getCellState(chosenX, chosenY) == currentPlayer.playerId) {
+                            //checkMoves(chosenX, chosenY, playerId);
+
+                        } else {
+                            output.println("MESSAGE Choose your pawn!");
+                        }
+
+                    }
+                    else {
+                        output.println("MESSAGE Not your turn");
+                    }
+                }
+                else if (command.startsWith("PASS")) {
+                    if (this == currentPlayer) {
+                        nextPlayer();
+                    }
+                    else {
+                        output.println("MESSAGE Not your turn");
+                    }
                 }
             }
         }
+
+        private void nextPlayer() {
+            if(this.playerId == players.size() - 1) {
+                currentPlayer = players.get(0);
+            }
+            else {
+                currentPlayer = players.get(this.playerId + 1);
+            }
+        }
+
+        private void processMoveCommand(int x, int y) {
+
+
+            GameRule rule = new MoveOneRule();
+            Cell currentCell = new Cell(x, y, playerId);
+            Iterator iterator = new RulesIterator(rule.getPossibleMoves(currentCell, board));
+            while(iterator.hasNext()) {
+                Change c = (Change)iterator.next();
+                if(c.getX() == newX && c.getY() == newY) {
+                    board.getCell(chosenX, chosenY).setCellState(currentPlayer.playerId);
+                    break;
+                }
+            }
+        }
+
     }
 
 
