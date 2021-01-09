66d683694ecc0325fde92e706e35670ab11e6f44
KrzysztofT415 <Krzysztof15042000@wp.pl>
2021-01-09 16:24:19 +0100

    Added documentation for both mains (client and server).

--- ChineseCheckersClient/src/main/java/appClient/MainClient.java
+++ ChineseCheckersClient/src/main/java/appClient/MainClient.java
@@ -8,37 +8,59 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+/**
+ * A client for a network multi-player Chinese Checkers game.
+ * Window of application is made using Swing but is made to be easily prescribed using other GUI libraries.
+ * After compiling user will be asked to enter ip of server to connect to.
+ *
+ * Communication between client and server is based on plain text, called as ChChP (Chinese Checkers Protocol)
+ * The strings that are sent in ChChP are:
+ *
+ *  Client -> Server           Server -> Client
+ *  ----------------           ----------------
+ *  CLICK [x;y]                WELCOME [n] (1 <= n <= 6)
+ *  PASS                       START [n;x1;y1;s1;x2;y2...]
+ *  QUIT                       SET [n;x1;y1;s1;x2;y2...]
+ *                             CLEAR
+ *                             MESSAGE [String]
+ *                             LOST
+ *                             WON [n] (1 <= n <= 6)
+ *                             LEAVE
+ *                             END
+ *
+ * where:
+ * in CLICK [x;y] are coordinates of clicked cell
+ * in SET and START [n;x1;y1;s1;x2;y2...]:
+ *   <n> means number of changed cells
+ *   x[i], y[i] are coordinates of changed cell
+ *   s[i] is new state of this cell
+ *   where 0 < [i] <= <n>
+ * in WELCOME [n] is playerId of player that joined
+ * in WON [n] is playerId of player that won game
+ * in MESSAGE [String] is text to be displayed in message box on client
+ *
+ * @author Martyna Dziamara, Krzysztof Tałałaj
+ */
 public class MainClient {
 
-    private Socket socket;
-    private AppWindow appWindow;
-    private ClientCommunicationService communicationService;
+    private final Socket socket;
+    private final AppWindow appWindow;
+    private final ClientCommunicationService communicationService;
 
-
-    public MainClient() {
-        while(true) {
-            String input = JOptionPane.showInputDialog("Ip address of server host (ip:port) :");
-            if (input == null) break;
-
-            try {
-                String ip = input.split(":")[0];
-                int port = Integer.parseInt(input.split(":")[1]);
-
-                this.socket = new Socket(InetAddress.getByName(ip), port);
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Connection refused, try again");
-                continue;
-            }
-            System.out.println("Client connected to : "+input);
-
-            this.communicationService = new ClientCommunicationService(this, this.socket);
-            this.communicationService.connectModule(new GameCommunicationModule(this));
-            this.appWindow = new SwingAppWindow(this);
-            this.start();
-            break;
-        }
+    /**
+     * Verifies arguments and then creates lobby for expected players.
+     * @param socket server socket that client is connected to
+     */
+    public MainClient(Socket socket) {
+        this.socket = socket;
+        this.communicationService = new ClientCommunicationService(this, this.socket);
+        this.communicationService.connectModule(new GameCommunicationModule(this));
+        this.appWindow = new SwingAppWindow(this);
     }
 
+    /**
+     * Runs the client communication with server.
+     */
     public void start() {
         try {
             communicationService.start();
@@ -51,6 +73,30 @@ public class MainClient {
         }
     }
 
+    /**
+     * Runs the application. Verifies connection and creates new Client.
+     */
+    public static void main(String[] args) {
+        while(true) {
+            String input = JOptionPane.showInputDialog("Ip address of server host (ip:port) :");
+            if (input == null) break;
+
+            try {
+                String ip = input.split(":")[0];
+                int port = Integer.parseInt(input.split(":")[1]);
+
+                Socket socket = new Socket(InetAddress.getByName(ip), port);
+                MainClient mainClient = new MainClient(socket);
+
+                System.out.println("Client connected to : "+input);
+                mainClient.start();
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Connection refused, try again");
+            }
+        }
+    }
+
     public AppWindow getAppWindow() {
         return this.appWindow;
     }
@@ -59,7 +105,4 @@ public class MainClient {
         return this.communicationService;
     }
 
-    public static void main(String[] args) {
-        new MainClient();
-    }
 }
--- ChineseCheckersServer/src/main/java/appServer/MainServer.java
+++ ChineseCheckersServer/src/main/java/appServer/MainServer.java
@@ -2,24 +2,70 @@ package appServer;
 
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * A server for a network multi-player Chinese Checkers game.
+ *
+ * !important! While compiling pass as argument number of players that are expected to be in game
+ *             correct number of players are : 2, 3, 4, 6
+ *
+ * Communication between server and clients is based on plain text, called as ChChP (Chinese Checkers Protocol)
+ * The strings that are sent in ChChP are:
+ *
+ *  Client -> Server           Server -> Client
+ *  ----------------           ----------------
+ *  CLICK [x;y]                WELCOME [n] (1 <= n <= 6)
+ *  PASS                       START [n;x1;y1;s1;x2;y2...]
+ *  QUIT                       SET [n;x1;y1;s1;x2;y2...]
+ *                             CLEAR
+ *                             MESSAGE [String]
+ *                             LOST
+ *                             WON [n] (1 <= n <= 6)
+ *                             LEAVE
+ *                             END
+ *
+ * where:
+ * in CLICK [x;y] are coordinates of clicked cell
+ * in SET and START [n;x1;y1;s1;x2;y2...]:
+ *   <n> means number of changed cells
+ *   x[i], y[i] are coordinates of changed cell
+ *   s[i] is new state of this cell
+ *   where 0 < [i] <= <n>
+ * in WELCOME [n] is playerId of player that joined
+ * in WON [n] is playerId of player that won game
+ * in MESSAGE [String] is text to be displayed in message box on client
+ *
+ * @author Martyna Dziamara, Krzysztof Tałałaj
+ */
 public class MainServer implements Runnable {
 
     private final int lobbySize;
 
+    /**
+     * Acts as lobby for players. Waits until all players are connected.
+     * After all players joined sends information about game starting to all clients.
+     * @param lobbySize number of players to join
+     */
     public MainServer(int lobbySize) {
         this.lobbySize = lobbySize;
     }
 
+    /**
+     * Runs the lobby on localhost and random port.
+     * Prints ip to which players should connect.
+     * Also prints information about current connecting-events into terminal.
+     */
     @Override
     public void run() {
         try {
-            try (ServerSocket listener = new ServerSocket(58901)) {
+            int port = new Random().nextInt(10000);
+            try (ServerSocket listener = new ServerSocket(port)) {
 
                 System.out.println("Chinese Checkers Server is Running");
-                System.out.println("Server ip : " + InetAddress.getLocalHost().getHostAddress() + ":58901");
+                System.out.println("Server ip : " + InetAddress.getLocalHost().getHostAddress() + ":" + port);
 
                 ExecutorService pool = Executors.newFixedThreadPool(lobbySize);
 
@@ -33,7 +79,7 @@ public class MainServer implements Runnable {
                     game.connectPlayer(newPlayer, i - 1);
                     pool.execute(newPlayer);
 
-                    System.out.println("app.Player " + i + " joined");
+                    System.out.println("Player " + i + " joined");
                 }
                 synchronized (this) {
                     wait(100);
@@ -47,6 +93,12 @@ public class MainServer implements Runnable {
         }
     }
 
+    /**
+     * Verifies arguments.
+     * @param args arguments to be verified
+     * @return number of players
+     * @throws IllegalArgumentException when arguments are incorrect
+     */
     public static int verifyArguments(String[] args) throws IllegalArgumentException {
         if (args.length != 1) { throw new IllegalArgumentException("Incorrect number of arguments"); }
 
@@ -61,6 +113,9 @@ public class MainServer implements Runnable {
         return numberOfPlayers;
     }
 
+    /**
+     * Runs the application. Verifies arguments and then creates lobby for expected players.
+     */
     public static void main(String[] args) {
         int numberOfPlayers;
         try { numberOfPlayers = MainServer.verifyArguments(args); }
