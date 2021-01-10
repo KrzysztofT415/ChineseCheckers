803bece1166d4aba29c26afcd48eafb3bae283df
martyna-dz <martynadziamara14@gmail.com>
2021-01-10 13:56:07 +0100

    changed junit tests for class MainClient

--- ChineseCheckersClient/src/test/java/appClient/MainClientTest.java
+++ ChineseCheckersClient/src/test/java/appClient/MainClientTest.java
@@ -3,7 +3,10 @@ package appClient;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import view.AppWindow;
+
+import java.lang.reflect.Field;
 
 import static org.mockito.Mockito.*;
 
@@ -14,10 +17,18 @@ public class MainClientTest {
     ClientCommunicationService communicationService;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IllegalAccessException, NoSuchFieldException {
         client = mock(MainClient.class);
-        when(client.getAppWindow()).thenReturn(appWindow);
-        when(client.getCommunicationService()).thenReturn(communicationService);
+
+        Field window = MainClient.class.getDeclaredField("appWindow");
+        window.setAccessible(true);
+        window.set(client, appWindow);
+        Mockito.doCallRealMethod().when(client).getAppWindow();
+
+        Field communication = MainClient.class.getDeclaredField("communicationService");
+        communication.setAccessible(true);
+        communication.set(client, communicationService);
+        Mockito.doCallRealMethod().when(client).getCommunicationService();
     }
 
     @Test
